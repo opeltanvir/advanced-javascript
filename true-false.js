8161d6395aabb962aa8dd699588a893943593bf2
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-21 22:30:07 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// falsey
+// false
+// 0
+// ""
+// undenfined
+// null
+// NaN
+// Truthy
+// '0' ' ' []
+
+
+let name =0;
+
+if (name || name ===0){
+    console.log("consition is true");
+}
+else{
+    console.log("condition is false");
+}
