53c689812b9d11867888a31d1523fcfc04086e22
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-22 22:06:09 +0600

    apply

--- callApply.js
+++ callApply.js
@@ -1,12 +1,12 @@
 const normalPerson = {
     firstName:'Rahim',
     lastName :'Uddin',
-    salary : 2000,
+    salary : 5000,
     getFullName:function(){
          console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
-        this.salary = this.salary -amount;
+    chargeBill: function(amount ,tips ,tax){
+        this.salary = this.salary -amount -tips -tax;
         return this.salary;
     }
 }
@@ -14,15 +14,23 @@ const normalPerson = {
 const heroPerson ={
     firstName: 'hero',
     lastName: 'balam',
-    salary: 2500
+    salary: 5000
 }
 const friendlyPerson ={
     firstName: 'hero',
     lastName: 'golam',
-    salary: 2500
+    salary: 25000
 }
 
-const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
-heroBillCharge(200);
+// const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+// heroBillCharge(200);
+// console.log(heroPerson);
+// normalPerson.chargeBill.call(heroPerson, 900,100,10);
+// console.log(heroPerson.salary);
+// normalPerson.chargeBill.call(friendlyPerson, 5000, 500, 50);
+// console.log(friendlyPerson.salary);
+normalPerson.chargeBill.apply(heroPerson, [3000 , 300,30]);
+console.log(heroPerson.salary);
+
 
 
