6da2bcd25086928bc7dadf9393ae6827dd09794b
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-23 00:09:07 +0600

    timeout

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomething(){
+    console.log(3333);
+}
+setInterval(function(){
+    console.log('doing it')
+} ,7777)
+// console.log(2222);
+// setTimeout(() => {
+//     console.log('see you later')
+// },4000)
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
